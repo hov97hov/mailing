7593fea0007fb218e88f5eb2d191609e6e8bfd2c
hov97hov <hovo1997mkrtchyan@gmail.com>
2023-04-06 01:34:37 +0400

    test

--- resources/views/email/template1.blade.php
+++ resources/views/email/template1.blade.php
@@ -8,31 +8,67 @@
     <title>Document</title>
 </head>
 <body>
-   <div style="min-width: 320px">
-        <div style="text-align: center">
-        <div>
+<div style="min-width: 320px">
+    <div style="text-align: center">
+        <div style="margin-bottom: 40px">
             <a href="{{$data['imgLink']}}">
                 <img style="max-width: 100%" src="https://ymail.yerevak.am{{$data['image'][1]}}" alt="">
             </a>
         </div>
     </div>
-    <br>
-    <div>
+    <div style="margin-bottom: 40px; text-align: center">
         @if($files)
             @foreach($files as $file)
-                ֆայլ - <a target="_blank" href="https://ymail.yerevak.am{{$file[1]}}" download>{{$file[0]}}</a> <br> <br>
+                <a
+                    style="
+                    font-family: 'Arial AMU';
+                    font-style: normal;
+                    font-weight: 400;
+                    font-size: 22px;
+                    line-height: 30px;
+                    text-align: center;
+                    color: #392E2C;
+                    padding: 5px;
+                    border: 1px solid #6E6E6E;
+                    border-radius: 5px;
+                    display: inline-flex;
+                    align-items: center;
+                    text-decoration: none;
+                   "
+                    target="_blank" href="https://ymail.yerevak.am{{$file[1]}}" download
+                >
+                    <img style="margin-right: 10px" src="https://ymail.yerevak.am/images/download.png">
+                    <span>Ներբեռնել ֆայլը</span>
+                </a>
             @endforeach
         @endif
     </div>
-    <br>
-    <br>
-    <div>
+    @if($data['mailingImg'] != 'null')
+        <div style="width: 80%; height: 2px; float: right; background: #D9D9D9; margin-bottom: 10px"></div>
+    @endif
+    <div style="float: right">
         @if($data['mailingImg'] != 'null')
-            <a href="{{$data['bottom_img_link']}}" target="_blank">
-                <img src="https://ymail.yerevak.am/images/mailing.png" alt="">
+            <a href="{{$data['bottom_img_link']}}" style="
+                padding: 5px 10px;
+                background: #DDE8F8;
+                border-radius: 3px;
+                font-family: 'Arial AMU';
+                text-decoration: none;
+                font-style: normal;
+                font-weight: 700;
+                font-size: 18px;
+                line-height: 16px;
+                text-align: center;
+                color: rgba(37, 50, 102, 0.75);
+                display: flex;
+                align-items: center;
+              "
+               target="_blank">
+                <img style="margin-right: 10px" src="https://ymail.yerevak.am/images/ok.png" alt="">
+                <span style="margin-top: 10px">Ուղարկեք հաղորդագրություն 30․000 էլ․ հասցեների</span>
             </a>
         @endif
     </div>
-   </div>
+</div>
 </body>
 </html>
--- resources/views/email/template4.blade.php
+++ resources/views/email/template4.blade.php
@@ -8,38 +8,98 @@
     <title>Document</title>
 </head>
 <body>
-    <div style="min-width: 320px">
-        <div style="text-align: center">
-        <div>
+<div style="min-width: 320px">
+    <div style="text-align: center">
+        <div style="margin-bottom: 20px">
             <img style="max-width: 100%" src="https://ymail.yerevak.am{{$data['image'][1]}}" alt="">
         </div>
-        <div style="margin: 30px 0">
+        <div style="
+            margin-bottom: 50px;
+            font-family: 'Arial AMU';
+            font-style: normal;
+            font-weight: 400;
+            font-size: 25px;
+            line-height: 33px;
+            color: #7C706D;
+            ">
             {!! $data['text'] !!}
         </div>
-        <br>
-        <br>
-        <a target="_blank" style="padding: 10px 30px; border-radius: 6px; border: 0; background: #253266; color: #ffffff; text-decoration: none" href="{{$data['btnLink']}}">
-            {{$data['btnName']}}
-        </a>
+
+        <div style=" margin-bottom: 40px">
+            <a target="_blank" style="
+                padding: 12px 22px;
+                border-radius: 6px;
+                border: 0;
+                background: #606060;
+                color: #ffffff;
+                text-decoration:none;
+                font-family: 'Arial AMU';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 30px;
+                line-height: 30px;
+                text-align: center;
+              "
+               href="{{$data['btnLink']}}"
+            >
+                {{$data['btnName']}}
+            </a>
+        </div>
     </div>
-    <br>
-    <div>
+    <div style="margin-bottom: 40px; text-align: center">
         @if($files)
             @foreach($files as $file)
-                ֆայլ - <a target="_blank" href="https://ymail.yerevak.am{{$file[1]}}" download>{{$file[0]}}</a> <br> <br>
+                <a
+                    style="
+                        font-family: 'Arial AMU';
+                        font-style: normal;
+                        font-weight: 400;
+                        font-size: 22px;
+                        line-height: 30px;
+                        text-align: center;
+                        color: #392E2C;
+                        padding: 5px;
+                        border: 1px solid #6E6E6E;
+                        border-radius: 5px;
+                        display: inline-flex;
+                        align-items: center;
+                        text-decoration: none;
+                       "
+                    target="_blank" href="https://ymail.yerevak.am{{$file[1]}}" download
+                >
+                    <img style="margin-right: 10px" src="https://ymail.yerevak.am/images/download.png">
+                    <span>Ներբեռնել ֆայլը</span>
+                </a>
             @endforeach
         @endif
     </div>
-    <br>
-    <br>
-    <div>
+    @if($data['mailingImg'] != 'null')
+        <div style="width: 80%; height: 2px; float: right; background: #D9D9D9; margin-bottom: 10px"></div>
+    @endif
+    <div style="float: right">
         @if($data['mailingImg'] != 'null')
-            <a target="_blank" href="{{$data['bottom_img_link']}}">
-                <img src="https://ymail.yerevak.am/images/mailing.png">
+            <a href="{{$data['bottom_img_link']}}" style="
+                padding: 5px 10px;
+                background: #DDE8F8;
+                border-radius: 3px;
+                font-family: 'Arial AMU';
+                text-decoration: none;
+                font-style: normal;
+                font-weight: 700;
+                font-size: 18px;
+                line-height: 16px;
+                text-align: center;
+                color: rgba(37, 50, 102, 0.75);
+                display: flex;
+                align-items: center;
+              "
+               target="_blank">
+                <img style="margin-right: 10px" src="https://ymail.yerevak.am/images/ok.png" alt="">
+                <span style="margin-top: 10px">Ուղարկեք հաղորդագրություն 30․000 էլ․ հասցեների</span>
             </a>
         @endif
     </div>
-    </div>
+</div>
 
 </body>
 </html>
